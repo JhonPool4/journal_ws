e9fb7de870d0fa865fe20e96d27e3113dc0e9eeb
JhonPool4 <jhoncharaja@gmail.com>
2022-01-22 08:10:14 -0300

    new version, images without padding

--- src/motion_ur5_rviz/matlab_code/compare_cartesian_tracking_error.m
+++ src/motion_ur5_rviz/matlab_code/compare_cartesian_tracking_error.m
@@ -4,12 +4,12 @@
 clc, clear all, close all;
 
 pwd = '/home/jhon/catkin_ws/journal_ws/src/motion_ur5_rviz/';
-control_method = 'PDi'; % change: (i) PDi or SMCi 
+control_method = 'SMCi'; % change: (i) PDi or SMCi 
 trajectory_type  = '/circular_traj/'; % change to database
-file_name_1 = 'cartesian_KP_9_KD_6_alpha_0.0_beta_0.1_gamma_0.9_delta_0.1_t_50.csv';
-file_name_2 = 'cartesian_KP_9_KD_6_alpha_0.5_beta_0.1_gamma_0.9_delta_0.1_t_50.csv';
-%file_name_1 = 'cartesian_L5_K_1_alpha_0.0_beta_0.1_gamma_0.9_delta_0.1_t_50.csv';
-%file_name_2 = 'cartesian_L5_K_1_alpha_0.01_beta_0.1_gamma_0.9_delta_0.1_t_50.csv';
+%file_name_1 = 'cartesian_KP_9_KD_6_alpha_0.0_beta_0.1_gamma_0.9_delta_0.1_t_50.csv';
+%file_name_2 = 'cartesian_KP_9_KD_6_alpha_0.5_beta_0.1_gamma_0.9_delta_0.1_t_50.csv';
+file_name_1 = 'cartesian_L5_K_1_alpha_0.0_beta_0.1_gamma_0.9_delta_0.1_t_50.csv';
+file_name_2 = 'cartesian_L5_K_1_alpha_0.01_beta_0.1_gamma_0.9_delta_0.1_t_50.csv';
 file_path = fullfile(pwd, 'data/',control_method, trajectory_type);
 image_path = fullfile(pwd,'document/images/', control_method, trajectory_type);
 
@@ -23,138 +23,62 @@ t_start = 1;
 t_step  = 5;
 t_end   = 3001;%length(time);
 t_dt = (time(t_end) - time(t_start))/3;
-
-
-% get cartesian data
+      
+% get articular data
 [p_med_1, dp_med_1, ddp_med_1, p_ref_1, dp_ref_1, ddp_ref_1, p_e_1, dp_e_1, ddp_e_1] = get_cartesian_position_data( data_1, t_start, t_step, t_end);
 [rpy_med_1, rpy_ref_1, rpy_e_1] = get_cartesian_orientation_data(data_1, t_start, t_step, t_end);
 
 [p_med_2, dp_med_2, ddp_med_2, p_ref_2, dp_ref_2, ddp_ref_2, p_e_2, dp_e_2, ddp_e_2] = get_cartesian_position_data( data_2, t_start, t_step, t_end);
 [rpy_med_2, rpy_ref_2, rpy_e_2] = get_cartesian_orientation_data(data_2, t_start, t_step, t_end);
 
-% get control parameters
-[K1_1, K2_1, K1_2, K2_2] = get_control_parameters(control_method, data_2, t_start, t_step, t_end);
 
-% colors
-color1 = [0, 0.4470, 0.7410];                % dark red        --alpha=0.1 
-color2 = [0.6350, 0.0780, 0.1840];       % dark blue      --alpha=0.5
-color3 = [0.4660, 0.6740, 0.1880];       % dark green    --alpha=0.8
+x_data = time(t_start:t_step:t_end);
+y_data_1 = [p_e_1(:,1)';p_e_1(:,2)';p_e_1(:,1)' ; ddp_e_1(:,1)';ddp_e_1(:,2)';ddp_e_1(:,3)'; rpy_e_1(:,1)'; rpy_e_1(:,2)'; rpy_e_1(:,3)'];
+y_data_2 = [p_e_2(:,1)';p_e_2(:,2)';p_e_2(:,1)' ; ddp_e_2(:,1)';ddp_e_2(:,2)';ddp_e_2(:,3)'; rpy_e_2(:,1)'; rpy_e_2(:,2)'; rpy_e_2(:,3)'];
 
-% page size
-image_size_height = 5;
-image_size_height_legend = 1.5*5;
-image_size_width = 17;
-%% cartesian position: tracking error
-clc, close all;
+%%
 
-figure(1), hold on, grid on, box on;
-    set(gcf,'units','centimeters','position', [0 0 image_size_width image_size_height_legend])
+close all
+tiledlayout(3,3, 'Padding', 'none')
 
-name_list = ["$\mathrm{x}$", "$\mathrm{y}$","$\mathrm{z}$"];
+name_list_1 = ["$\mathrm{x}$", "$\mathrm{y}$","$\mathrm{z}$"];
+name_list_2 = ["$\mathrm{\ddot{x}}$", "$\mathrm{\ddot{y}}$","$\mathrm{\ddot{z}}$"];
+name_list_3 = ["$\mathrm{\phi_{roll}}$", "$\mathrm{\phi_{pitch}}$","$\mathrm{\phi_{roll}}$"];
 
-cnt_img = 3;
-space_size = 1;
-img_size = 3;
-rows = img_size + 2;
-cols  = img_size*cnt_img+space_size*(cnt_img-1);
-legend_flag = true;
+unit_list_1 = [" ($\mathrm{mm}$)", " ($\mathrm{mm}$)", " ($\mathrm{mm}$)"];
+unit_list_2 = [" ($\mathrm{\frac{mm}{s^2}}$)", " ($\mathrm{\frac{mm}{s^2}}$)", " ($\mathrm{\frac{mm}{s^2}}$)"];
+unit_list_3 = [" ($\mathrm{^{\circ}}$)", " ($\mathrm{^{\circ}}$)", " ($\mathrm{^{\circ}}$)"];
+name_list = [name_list_1, name_list_2, name_list_3];
+unit_list  = [unit_list_1, unit_list_2, unit_list_3];
 
-for i=1:3
-    plot_name = strcat(name_list(i),' ($\mathrm{mm}$)');
-    subplot_indexes = get_subplot_index(i, rows,  cols, img_size, space_size, legend_flag);
-    subplot(rows, cols, subplot_indexes),
-    plot(time(t_start:t_step:t_end), p_e_1(:, i), 'linestyle', '-', 'linewidth', 2, 'color', color1), hold on, grid on, box on
-    plot(time(t_start:t_step:t_end), p_e_2(:, i), 'linestyle', '--','linewidth', 2, 'color', color2), hold on, grid on, box on
-    xlabel('time (s)', 'interpreter', 'latex')
-    ylabel(plot_name, 'interpreter', 'latex')
+for img_index=1:9
+    nexttile
+    plot_name = strcat(name_list(img_index), unit_list(img_index));
+    plot(x_data, y_data_1(img_index,:),'linestyle', '-', 'linewidth', 2), hold on, grid on, box on
+    plot(x_data, y_data_2(img_index,:),'linestyle', '-', 'linewidth', 2)
+    xlabel('Time (s)', 'Interpreter','latex'),
+    ylabel(plot_name, 'Interpreter','latex'),
     xticks(time(t_start):t_dt:time(t_end))
-    xlim([0 time(t_end)])
+    xlim([0 time(t_end)])    
     ax = gca; % current axes
-    ax.FontSize = 11;
-    set(gca,'TickLabelInterpreter','latex')
-    
-end         
+    ax.FontSize = 10;
+    set(gca,'TickLabelInterpreter','latex')   
+    if img_index==1
+        title(" ", "FontSize",40)
+    end
+        
+end 
+
+chosen_figure=gcf;
+set(chosen_figure,'PaperUnits','centimeters');
+set(chosen_figure,'PaperPositionMode','auto');
+set(chosen_figure,'Units','centimeters');
+
+set(chosen_figure,'Position',[0 0 16 20]);
 
 % add legend
 Lgnd = legend({'fixed gains', 'with optimization'}, 'interpreter', 'latex', 'Orientation','horizontal');
 Lgnd.FontSize = 12;
 Lgnd.Position(1) = 0.3;
-Lgnd.Position(2) = 0.85;
-    
-
-% Save image
-file_name     = fullfile(image_path, 'cartesian_position_error');
-saveas(gcf,file_name,'epsc')  
-
-
-%% cartesian velocity: tracking error
-clc, close all;
-
-figure(1), hold on, grid on, box on;
-    set(gcf,'units','centimeters','position', [0 0 image_size_width image_size_height])
-
-name_list = ["$\mathrm{\dot{x}}$", "$\mathrm{\dot{y}}$","$\mathrm{\dot{z}}$"];
-
-cnt_img = 3;
-space_size = 1;
-img_size = 3;
-rows = img_size + 2;
-cols  = img_size*cnt_img+space_size*(cnt_img-1);
-legend_flag = false;
-
-for i=1:3
-    plot_name = strcat(name_list(i),' ($\mathrm{\frac{mm}{s}}$)');
-    subplot_indexes = get_subplot_index(i, rows,  cols, img_size, space_size, legend_flag);
-    subplot(rows, cols, subplot_indexes),
-    plot(time(t_start:t_step:t_end), dp_e_1(:, i), 'linestyle', '-', 'linewidth', 2, 'color', color1), hold on, grid on, box on
-    plot(time(t_start:t_step:t_end), dp_e_2(:, i), 'linestyle', '--','linewidth', 2, 'color', color2), hold on, grid on, box on
-    xlabel('time (s)', 'interpreter', 'latex')
-    ylabel(plot_name, 'interpreter', 'latex')
-    xticks(time(t_start):t_dt:time(t_end))
-    xlim([0 time(t_end)])
-    ax = gca; % current axes
-    ax.FontSize = 12;
-    set(gca,'TickLabelInterpreter','latex')
-    
-end           
-
-% Save image
-file_name     = fullfile(image_path, 'cartesian_velocity_error');
-saveas(gcf,file_name,'epsc')  
-
-
-%% cartesian orientation with Euler angles: tracking error
-clc, close all;
-
-figure(1), hold on, grid on, box on;
-    set(gcf,'units','centimeters','position', [0 0 image_size_width image_size_height])
-
-name_list = ["$\mathrm{\phi_{roll}}$", "$\mathrm{\phi_{pitch}}$","$\mathrm{\phi_{roll}}$"];
-
-cnt_img = 3;
-space_size = 1;
-img_size = 3;
-rows = img_size + 2;
-cols  = img_size*cnt_img+space_size*(cnt_img-1);
-legend_flag = false;
-
-for i=1:3
-    plot_name = strcat(name_list(i),' ($\mathrm{^{\circ}}$)');
-    subplot_indexes = get_subplot_index(i, rows,  cols, img_size, space_size, legend_flag);
-    subplot(rows, cols, subplot_indexes),
-    plot(time(t_start:t_step:t_end), rpy_e_1(:, i), 'linestyle', '-', 'linewidth', 2, 'color', color1), hold on, grid on, box on
-    plot(time(t_start:t_step:t_end), rpy_e_2(:,i), 'linestyle', '--','linewidth', 2, 'color', color2), hold on, grid on, box on
-    xlabel('time (s)', 'interpreter', 'latex')
-    ylabel(plot_name, 'interpreter', 'latex')
-    xticks(time(t_start):t_dt:time(t_end))
-    xlim([0 time(t_end)])
-    ax = gca; % current axes
-    ax.FontSize = 12;
-    set(gca,'TickLabelInterpreter','latex')
-    
-end           
-
-% Save image
-file_name     = fullfile(image_path, 'cartesian_rpy_error');
-saveas(gcf,file_name,'epsc')  
-
+Lgnd.Position(2) = 0.95;
+saveas(chosen_figure,'/home/jhon/Desktop/filename', 'epsc'); %Set desired file name
