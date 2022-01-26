ebaee74b9ee973f6b2b2eca54fb61f9d2a8e3d59
JhonPool4 <jhoncharaja@gmail.com>
2022-01-26 15:52:37 -0300

    function to generate figures with specific features

---
+++ src/motion_ur5_rviz/matlab_code/generate_figure.m
@@ -0,0 +1,48 @@
+function generate_figure(y_data_1, y_data_2, x_data, fc)
+
+% time variables
+time = fc.time;
+t_start = fc.t_start;
+t_end   = fc.t_end;
+t_dt = fc.t_dt;
+
+
+tiledlayout(fc.cnt_v_img,fc.cnt_h_img, 'Padding', 'none')
+
+for img_index=1:fc.cnt_v_img*fc.cnt_h_img
+    nexttile
+    plot_name = strcat(fc.name_list(img_index), fc.unit_list(img_index));
+    plot(x_data, y_data_1(img_index,:),'linestyle', fc.ls_list{1}, 'linewidth', fc.lw_list{1}, 'color', fc.color_list{1}), hold on, grid on, box on
+    plot(x_data, y_data_2(img_index,:),'linestyle', fc.ls_list{2}, 'linewidth', fc.lw_list{2}, 'color', fc.color_list{2})
+    xlabel('Time (s)', 'Interpreter','latex'),
+    ylabel(plot_name, 'Interpreter','latex'),
+    xticks(time(t_start):t_dt:time(t_end))
+    xlim([time(t_start) time(t_end)])    
+    ax = gca; % current axes
+    ax.FontSize = fc.axis_font_size;
+    set(gca,'TickLabelInterpreter','latex')   
+    if (img_index==1 && fc.legend_flag==1)
+        title(" ", "FontSize",40)
+    end
+        
+end 
+
+chosen_figure=gcf;
+set(chosen_figure,'PaperUnits','centimeters');
+set(chosen_figure,'PaperPositionMode','auto');
+set(chosen_figure,'Units','centimeters');
+
+set(chosen_figure,'Position',[0 0 fc.img_width fc.img_height]);
+
+% add legend
+if (fc.legend_flag==1)
+    Lgnd = legend({'fixed gains', 'with optimization'}, 'interpreter', 'latex', 'Orientation','horizontal');
+    Lgnd.FontSize = fc.legend_font_size;
+    Lgnd.Position(1) = fc.legend_xpos;
+    Lgnd.Position(2) = fc.legend_ypos;
+end
+
+% save figure
+saveas(chosen_figure, fc.save_path, 'epsc'); %Set desired file name
+
+end
