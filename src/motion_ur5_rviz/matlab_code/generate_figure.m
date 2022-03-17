8a60dafe7bd6d8afdf0f4d613ed00a14d9869c97
JhonPool4 <jhoncharaja@gmail.com>
2022-03-16 23:02:33 -0300

    to compare three signals

--- src/motion_ur5_rviz/matlab_code/generate_figure.m
+++ src/motion_ur5_rviz/matlab_code/generate_figure.m
@@ -1,4 +1,4 @@
-function generate_figure(y_data_1, y_data_2, x_data, fc)
+function generate_figure(y_data_1, y_data_2, y_data_3, x_data, fc)
 
 % time variables
 time = fc.time;
@@ -13,7 +13,9 @@ for img_index=1:fc.cnt_v_img*fc.cnt_h_img
     nexttile
     plot_name = strcat(fc.name_list(img_index), fc.unit_list(img_index));
     plot(x_data, y_data_1(img_index,:),'linestyle', fc.ls_list{1}, 'linewidth', fc.lw_list{1}, 'color', fc.color_list{1}), hold on, grid on, box on
-    plot(x_data, y_data_2(img_index,:),'linestyle', fc.ls_list{2}, 'linewidth', fc.lw_list{2}, 'color', fc.color_list{2})
+    plot(x_data, y_data_2(img_index,:),'linestyle', fc.ls_list{2}, 'linewidth', fc.lw_list{2}, 'color', fc.color_list{2}), hold on, grid on, box on
+    plot(x_data, y_data_3(img_index,:),'linestyle', fc.ls_list{3}, 'linewidth', fc.lw_list{3}, 'color', fc.color_list{3})
+    
     xlabel('Time (s)', 'Interpreter','latex'),
     ylabel(plot_name, 'Interpreter','latex'),
     xticks(time(t_start):t_dt:time(t_end))
@@ -38,8 +40,8 @@ set(chosen_figure,'Position',[0 0 fc.img_width fc.img_height]);
 if (fc.legend_space~=0)
     Lgnd = legend(fc.legend_list, 'interpreter', 'latex', 'Orientation','horizontal');
     Lgnd.FontSize = fc.legend_font_size;
-    Lgnd.Position(1) = fc.legend_xpos;
-    Lgnd.Position(2) = fc.legend_ypos;
+    Lgnd.Position(1) = 0.5 - Lgnd.Position(3)/2; %fc.legend_xpos;
+    Lgnd.Position(2) = 0.95- Lgnd.Position(4)/2;
 end
 
 % save figure
